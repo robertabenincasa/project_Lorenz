5b91b9260b0435657f058b4ffd9ff3f1cd7cc4b4
Roberta Benincasa <roberta.benincasa@studio.unibo.it>
2022-08-21 20:17:22 +0200

    first function completed

---
+++ plots.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 21 13:40:54 2022
+
+@author: Lenovo
+"""
+
+import matplotlib.pyplot as plt
+
+def xzgraph(sol, r):
+    """ This function produces a plot of the solution of the integration of the 
+    Lorenz system in the plane x, z. 
+        
+        Arguments:
+        ----------
+        
+            sol : ndarray-like(float) 
+            It is the trajectory of the system in the 3 directions.
+            
+            r: scalar(float)
+            The value of the parameter r of the Lorenz system used in the 
+            simulation, i.e. the Rayleigh number. 
+            
+        Notes:
+        ------
+        The argument r is treated here as a variable in order to obtain the 
+        graph of the trajectory for each value of r used in the simulation.
+        
+        Raises:
+        -------
+        ValueError if the 2 components of the solutions do not have the same 
+        first dimension.
+            
+    """
+    rho = r
+    fig,(ax)=plt.subplots(1,1,figsize=(8,6))
+    ax.plot(sol[:,0], sol[:,2],'k', marker='.',markersize=1, label='L(IC1,t)')
+    ax.set_ylim([0,50])
+    ax.legend(loc='best')
+    ax.set_title('Solution of the numerical integration - r = %i'%rho)
+    ax.set_xlabel('x')
+    ax.set_ylabel('z')
+    ax.grid()
+    plt.show()
