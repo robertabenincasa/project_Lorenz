3b97130637463bf9e7dd25f11b103d2d5682b05f
Roberta Benincasa <roberta.benincasa@studio.unibo.it>
2022-08-25 17:33:53 +0200

    configuration is ok

--- config.py
+++ config.py
@@ -6,21 +6,25 @@ Created on Sun Aug 21 17:11:48 2022
 """
 
 from configparser import ConfigParser
+import numpy as np
 config = ConfigParser()
+
+#eps = np.array([ 0. , 1E-5 , 1E-3 , 1.0 ])
+#IC0 = np.array([ 9. , 10. , 20. ])
 
 config.read('config.ini')
 config.add_section('Integration settings')
 config.set('Integration settings', 'num_steps', '12000')
 config.set('Integration settings', 'dt', '0.005')
-#config.add_section('Parameters')
-#config.set('Parameters', 'sigma', '10.0')
-#config.set('Parameters', 'b', '8./3.')
-#config.set('Parameters', 'r1', '28.0')
-#config.set('Parameters', 'r2', '9.0')
-#config.add_section('Initial condition')
-#config.set('Initial condition', 'IC', '9.,10.,20.')
-#config.add_section('Perturbations')
-#config.set('Perturbations', 'eps',  '1E-10,1E-5,1.0')
+config.add_section('Parameters')
+config.set('Parameters', 'sigma', '10.0')
+config.set('Parameters', 'b', str(8./3.))
+config.set('Parameters', 'r1', '28.0')
+config.set('Parameters', 'r2', '9.0')
+config.add_section('Initial condition')
+config.set('Initial condition', 'IC', '9. , 10. , 20.')
+config.add_section('Perturbations')
+config.set('Perturbations', 'eps', '0. , 1E-5 , 1E-3 , 1.0')
 
 with open('config.ini', 'w') as f:
     config.write(f)
