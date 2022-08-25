9c1ddaba324cb7a6ac9198e855ae1f5c4e3e9572
Roberta Benincasa <roberta.benincasa@studio.unibo.it>
2022-08-25 17:35:05 +0200

    improved quality of the figures

--- plots.py
+++ plots.py
@@ -36,29 +36,38 @@ def xzgraph(
         first dimension.
             
     """
-    rho = r
-    fig,(ax)=plt.subplots(1,1,figsize=(8,6))
-    ax.plot(sol[:,0], sol[:,2],'k', marker='.',markersize=1, label='L(IC1,t)')
+    
+    fig,(ax)=plt.subplots(1,1,figsize=(10,8))
+    ax.grid()
+    
+    ax.plot(sol[:,0], sol[:,2],'k', marker='.',markersize=1, label='L(IC0,t)')
+    ax.set_title('Solution of the numerical integration - r = %i'%r)
+    
     ax.set_ylim([0,50])
     ax.legend(loc='best')
-    ax.set_title('Solution of the numerical integration - r = %i'%rho)
+    
     ax.set_xlabel('x')
     ax.set_ylabel('z')
-    ax.grid()
+    
     plt.show()
     
 def plot_3dsolution(
         sol: np.ndarray,
         r: np.ndarray):
     
-    rho = r
-    fig = plt.figure()
+    fig = plt.figure(figsize = (10,8))
     ax = plt.axes(projection='3d')
-    ax.plot3D(sol[:,0], sol[:,1],sol[:,2], 'black', marker='.',markersize=0.2)
-    ax.set_title('Solution of the numerical integration - r = %i' %rho)
+    ax.grid()
+    
+    ax.plot3D(sol[:,0], sol[:,1],sol[:,2], 'black', marker='.',markersize=0.5)
+    ax.set_title('Solution of the numerical integration - r = %i' %r)
+   
+    
+    ax.set_xlabel('x', labelpad=20)
+    ax.set_ylabel('y', labelpad=20)
+    ax.set_zlabel('z', labelpad=20)
 
-
-
+    plt.show()
 
 
 
@@ -67,29 +76,48 @@ def plot_difference(
         t: np.ndarray,
         r: float):
     
-    rho = r
-    fig,(ax)=plt.subplots(1,1,figsize=(8,6))
-    ax.plot(t, diff,'k', marker='.',markersize=1, label='')
-    #ax.set_ylim([0,50])
-    ax.legend(loc='best')
-    ax.set_title('Difference - r = %i'%rho)
-    ax.set_xlabel('t')
+    
+    fig,(ax)=plt.subplots(1,1,figsize=(10,8))
     ax.grid()
+    
+    ax.plot(t, diff,'k', marker='.',markersize=1, label = 'L(IC0,t) - L(IC1,t)')
+    ax.set_title('Difference - r = %i'%r)
+    
+    ax.set_xlabel('t')
+    ax.legend(loc='best')
+    
     plt.show()
 
 def plot_rmse(
-        rsme: np.ndarray,
+        rmse: np.ndarray,
         t: np.ndarray,
         r: float,
         e: float,
         pred_time: float):
     
-    rho = r
-    fig,(ax)=plt.subplots(1,1,figsize=(8,6))
-    ax.plot(t, rsme,'k', marker='.',markersize=1, label='eps')
-    ax.axvline(pred_time, color = 'red', label = 'prediction time')
-    ax.legend(loc='best')
-    ax.set_title('Root Mean Square Error - r = %i'%rho)
-    ax.set_xlabel('t')
+    
+    fig,(ax,bx)=plt.subplots(2,1,figsize=(10,8))
+    plt.subplots_adjust(wspace=2, hspace=0.5)
     ax.grid()
+    bx.grid()
+    
+        
+    ax.plot(t, rmse,'k', marker='.',markersize=1, 
+            label='eps = '+ np.format_float_scientific(e))
+    ax.axvline(pred_time, color = 'red', 
+               label = 'prediction time = '+ np.format_float_scientific(pred_time))
+    ax.set_title('Root Mean Square Error - r = %i'%r)
+    
+    bx.semilogy(t, rmse,'k',marker='.',markersize=1, 
+                label='eps = '+ np.format_float_scientific(e))
+    bx.axvline(pred_time, color = 'red', 
+               label = 'prediction time = '+ np.format_float_scientific(pred_time))
+    bx.set_title('Root Mean Square Error - Log scale - r = %i'%r)
+    
+    ax.legend(loc='best')
+    ax.set_xlabel('t')
+    
+    bx.legend(loc='best')
+    bx.set_xlabel('t')
+    
     plt.show()
