7283d92c71a6336315169c1891bb6e2233f1b947
Roberta Benincasa <roberta.benincasa@studio.unibo.it>
2022-08-25 17:34:48 +0200

    new tests

--- integration.py
+++ integration.py
@@ -4,16 +4,20 @@ Created on Sun Aug 21 12:58:03 2022
 
 @author: roberta benincasa
 """
-from lorenz import (lorenz, perturbation, difference, RMSE, prediction)
-from plots import (xzgraph, plot_difference, plot_rmse, plot_3dsolution)
+from lorenz import (lorenz, perturbation, difference,
+                    RMSE, prediction, read_parameters)
+
+from plots import (xzgraph, plot_difference, plot_rmse,
+                   plot_3dsolution)
+
 from tabulate import tabulate
-import scipy.integrate
+#import scipy.integrate
 from scipy.integrate import odeint
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
 import configparser
-#from configparser import ConfigParser
+
 
 
 config = configparser.ConfigParser()
@@ -23,17 +27,21 @@ config.read('config.ini')
 #-----------------------Parameters-------------------------#
 #Canonical choice of the parameters of the Lorenz system,
 
-sigma = 10.
+sigma = config.get('Parameters', 'sigma')
+sigma = float(sigma)
 
-b = 8./3.
+b = config.get('Parameters', 'b')
+b = float(b)
 
-r1 = 28. #chaotic solution
+r1 = config.get('Parameters', 'r1') #chaotic solution
+r1 = float(r1)
 
-r2 = 9.  #non-chaotic solution
+r2 =  config.get('Parameters', 'r2') #non-chaotic solution
+r2 = float(r2)
 
 f1 = 0.
 
-f2 = 1.
+f2 = 1. 
 
 #-----------------Integration parameters-------------------#
 
@@ -43,12 +51,16 @@ num_steps = int(num_steps)
 dt = config.get('Integration settings', 'dt')
 dt = float(dt)
 
-IC0 =  [9.,10.,20.] #initial condition
+IC0 = config.get('Initial condition', 'IC') #initial condition
+IC0 = read_parameters(IC0)
 
-eps = [0.,1E-5,1E-3,1.] #perturbations
+eps = config.get('Perturbations', 'eps') #perturbations
+eps = read_parameters(eps)
+    
 
 t = np.linspace(0,num_steps,num_steps)*dt #time variable
-
+print(eps)
+print(IC0)
 
 #-----------------------Integration------------------------#
 
@@ -56,45 +68,48 @@ IC = perturbation(IC0,eps) #perturbed initial conditions
 
 #Initializing arrays
 
-sol_1 = np.zeros((num_steps , 3, len(eps))) #chaotic solution for each IC
-sol_2 = np.zeros((num_steps , 3, len(eps))) #non-chaotic solution for each IC
+#The following are the solution for each time step,
+#for each variable and for each IC
+sol_1 = np.zeros((num_steps , 3, len(eps))) 
+#chaotic solution 
+sol_2 = np.zeros((num_steps , 3, len(eps)))
+#non-chaotic solution 
 
-sol_1_f = np.zeros((num_steps , 3))
-sol_2_f = np.zeros((num_steps , 3))
 
 #Integrating
+
 #Solutions for each value of r and for each IC
+
 for i in range(len(eps)):
     
-    sol_1[:,:,i] = odeint(lorenz,IC[i,:],t,args=(sigma,b,r1,f1)) #chaotic solution
-    sol_2[:,:,i] = odeint(lorenz,IC[i,:],t,args=(sigma,b,r2,f1)) #non-chaotic solution
+    sol_1[:,:,i] = odeint(lorenz,IC[i,:],t,args=(sigma,b,r1,f1)) 
+    #chaotic solution
+    sol_2[:,:,i] = odeint(lorenz,IC[i,:],t,args=(sigma,b,r2,f1))
+    #non-chaotic solution
 
     
-
-
-#sol_1_f[:,:] = odeint(lorenz,IC[0,:],t,args=(sigma,b,r1,f2)) #chaotic solution
-#sol_2_f[:,:] = odeint(lorenz,IC[0,:],t,args=(sigma,b,r2,f2)) #non-chaotic solution
-
 
 #-------------------------Analysis-------------------------#
 
 #Initializing arrays
 
-delta_x = np.zeros((num_steps, 2)) #the difference was performed only 
-                                     #between the solution of unperturbed 
-                                     #case and the one of the first perturbed 
-                                     #case, as a preliminary analysis
+delta_x = np.zeros((num_steps, 2)) 
+#The difference was performed only between the solution of the unperturbed 
+#case and the one of the first perturbed case, as a preliminary analysis.
+#The difference was calculated for both chaotic and non-chaotic solution.
 
-error = np.zeros((num_steps, len(eps))) #the rmse was calculated for each
-                                         #perturbed case with r = 28
-pred_time = np.zeros(3)                  #the same for the prediction time 
+error = np.zeros((num_steps, len(eps))) 
+#The RMSE was calculated for each perturbed case with r = 28. 
+#The same for the prediction time.
+
+pred_time = np.zeros(3)                 
  
-#Calculating
 
-#the difference was calculated for both chaotic and non-chaotic solution
+#Calculating
 
 delta_x[:,0] = difference(sol_1[:,:,0], sol_1[:,:,1]) 
 delta_x[:,1] = difference(sol_2[:,:,0], sol_2[:,:,1])
+
 
 for i in range(1,len(eps)): 
     
@@ -107,27 +122,28 @@ pred_time = prediction(error, num_steps, dt, eps)
 
 #------------------------Plots & Tables--------------------------#
 
-    
-xzgraph(sol_1[:,:,0],r1) #plotting both chaotic and non-chaotic solution for
-xzgraph(sol_2[:,:,0],r2) #for the unpertubed case in the x,z plane
+#plotting both chaotic and non-chaotic solution for
+#the unpertubed case in the x,z plane
 
-#xzgraph(sol_2_f[:,:],r2)
-#xzgraph(sol_1_f[:,:],r1)
+xzgraph(sol_1[:,:,0],r1) 
+xzgraph(sol_2[:,:,0],r2) 
 
+#3D-plotting both chaotic and non-chaotic solution for
+#the unpertubed case 
 
 plot_3dsolution(sol_1[:,:,0],r1)
 plot_3dsolution(sol_2[:,:,0],r2)
 
-plot_difference(delta_x[:,0],t, r1) #plot the calculated differences
+plot_difference(delta_x[:,0],t, r1) 
 plot_difference(delta_x[:,1],t, r2)
 
-for i in range(1,len(eps)): #plot the rmse for each IC
+for i in range(1,len(eps)): 
    
     plot_rmse(error[:,i],t, r1, eps[i], pred_time[i-1])
 
 
-#creating a table with the values of the perturbation and of the corresponding
-#prediction times
+#creating a table with the values of the perturbation and
+# of the corresponding prediction times
 
 data = np.column_stack((eps[1:len(eps)], pred_time))
 
@@ -181,5 +197,18 @@ def test_RMSE1():
     for i in range(num_steps):
         
         assert rmse[i] == 0.
+        
+error1 = np.zeros((num_steps, len(eps)))
+error2 = np.ones((num_steps, len(eps)))
+
+def test_pred_time():
     
-            
+        time = prediction(error1, num_steps, dt, eps)
+        
+        assert np.all(time == 0.)
+        
+def test_pred_time1():
+    
+        time = prediction(error2, num_steps, dt, eps)
+        
+        assert np.all(time == 0.)
